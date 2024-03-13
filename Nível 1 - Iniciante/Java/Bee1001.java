9287d526da3eb491f4bd6d100843f393f5a8464a
joaov-sha <joaovitor8832@gmail.com>
2024-03-12 21:37:06 -0300

    Bee1001 - Java - success

---
+++ Nível 1 - Iniciante/Java/Bee1001.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Bee1001 {
+
+    public static Scanner s = new Scanner(System.in);
+    public static void main(String[] args){
+        int A,B;
+        A = s.nextInt();
+        B = s.nextInt();
+        System.out.println("X = " + (A+B));
+    }
+}
